5c7e4bf0446575f9d31a654d0880f5471d388ca5
udoyen <datameshprojects@gmail.com>
2025-12-07 13:55:25 +0100

    Updated title

--- src/app/page.js
+++ src/app/page.js
@@ -12,7 +12,7 @@ export default function Home() {
           className="rounded-full border-4 border-slate-500 shadow-lg"
         />
       </div>
-      <h1 className="text-4xl font-bold">Hello, I'm George Udosen</h1>
+      <h1 className="text-4xl font-bold">Hello, I'm George Udosen - Full Stack Developer</h1>
       <p className="mt-4 text-xl">Welcome to my Next.js Portfolio</p>
     </main>
   );
