705d4e5e12112135435bfc3ba4203cfc3bff5686
udoyen <datameshprojects@gmail.com>
2025-12-07 13:35:56 +0100

    Done with basic lessons on NextJS

--- src/app/page.js
+++ src/app/page.js
@@ -3,6 +3,15 @@ import Image from "next/image";
 export default function Home() {
   return (
    <main className="flex min-h-screen flex-col items-center p-24">
+      <div className="mb-8 relative">
+        <Image 
+          src="/profile.jpg" // Path starts from 'public' folder (don't write 'public/')
+          alt="Profile Picture"
+          width={200}        // Required: Helps browser reserve space
+          height={200}       // Required
+          className="rounded-full border-4 border-slate-500 shadow-lg"
+        />
+      </div>
       <h1 className="text-4xl font-bold">Hello, I'm George Udosen</h1>
       <p className="mt-4 text-xl">Welcome to my Next.js Portfolio</p>
     </main>
